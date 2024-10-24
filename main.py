d3b43caf7f2852dc42799713b051c0fb991a067e
quantrkucb <119884363+quantrkucb@users.noreply.github.com>
2024-10-25 02:14:41 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.header("Rishi Kumra: Recent Projects")
+
+st.write("""
+1. **Natural Language Processing on 10-K filings reports to forecast returns**
+   - Streamlit: [nlp10kfilingsrk.streamlit.app](https://nlp10kfilingsrk.streamlit.app/)
+   - GitHub: [github.com/quantrkucb/NLP_10K_Filings](https://github.com/quantrkucb/NLP_10K_Filings)
+
+2. **Portfolio Optimization: Analyzing and comparing models**
+   - Streamlit: [nlp10kfilingsrk.streamlit.app](https://nlp10kfilingsrk.streamlit.app/)
+   - GitHub: [github.com/quantrkucb/NLP_10K_Filings](https://github.com/quantrkucb/NLP_10K_Filings)
+
+3. **Tail-Risk hedging with financial derivatives (Bank of America)**
+   - Streamlit: [nlp10kfilingsrk.streamlit.app](https://nlp10kfilingsrk.streamlit.app/)
+   - GitHub: [github.com/quantrkucb/NLP_10K_Filings](https://github.com/quantrkucb/NLP_10K_Filings)
+   - Paper: [Link to Paper]()
+
+4. **Multi-asset return prediction with feature importance**
+   - Streamlit: [nlp10kfilingsrk.streamlit.app](https://nlp10kfilingsrk.streamlit.app/)
+   - GitHub: [github.com/quantrkucb/NLP_10K_Filings](https://github.com/quantrkucb/NLP_10K_Filings)
+   - Paper: [Link to Paper]()
+
+5. **Order Book Visualizer**
+   - Streamlit: [orderbookvisualiserrk.streamlit.app](https://orderbookvisualiserrk.streamlit.app/) 
+   - GitHub: [github.com/quantrkucb/OrderBookVisualiser](https://github.com/quantrkucb/OrderBookVisualiser)
+
+6. **[Cal Hackathon] Robo-advisor for retail investors**
+   - GitHub: [github.com/quantrkucb/CalHacksStockly](https://github.com/quantrkucb/CalHacksStockly/) 
+   - Website: [devpost.com/software/stockly](https://devpost.com/software/stockly)
+""")
