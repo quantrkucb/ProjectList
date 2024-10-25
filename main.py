f253059f96ef7932ca157b8c7475c3deff3da5ad
quantrkucb <119884363+quantrkucb@users.noreply.github.com>
2024-10-25 04:33:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,27 @@ st.write("""
    - Streamlit: [nlp10kfilingsrk.streamlit.app](https://nlp10kfilingsrk.streamlit.app/)
    - GitHub: [github.com/quantrkucb/NLP_10K_Filings](https://github.com/quantrkucb/NLP_10K_Filings)
 
-2. **Portfolio Optimization: Analyzing and comparing models**
-   - Streamlit: [nlp10kfilingsrk.streamlit.app](https://nlp10kfilingsrk.streamlit.app/)
-   - GitHub: [github.com/quantrkucb/NLP_10K_Filings](https://github.com/quantrkucb/NLP_10K_Filings)
+2. **Multi-asset return prediction with feature importance**
+   - GitHub (Paper + Slides): https://github.com/quantrkucb/MultiAssetReturnPrediction 
 
 3. **Tail-Risk hedging with financial derivatives (Bank of America)**
    - Streamlit: [nlp10kfilingsrk.streamlit.app](https://nlp10kfilingsrk.streamlit.app/)
    - GitHub: [github.com/quantrkucb/NLP_10K_Filings](https://github.com/quantrkucb/NLP_10K_Filings)
    - Paper: [Link to Paper]()
 
-4. **Multi-asset return prediction with feature importance**
+4.  **Order Book Visualizer**
+   - Streamlit: https://orderbookvisualiserrk.streamlit.app/ 
+   - GitHub: https://github.com/quantrkucb/OrderBookVisualiser 
+
+5. **Portfolio Optimization: Analyzing and comparing models**
    - Streamlit: [nlp10kfilingsrk.streamlit.app](https://nlp10kfilingsrk.streamlit.app/)
    - GitHub: [github.com/quantrkucb/NLP_10K_Filings](https://github.com/quantrkucb/NLP_10K_Filings)
-   - Paper: [Link to Paper]()
-
-5. **Order Book Visualizer**
-   - Streamlit: [orderbookvisualiserrk.streamlit.app](https://orderbookvisualiserrk.streamlit.app/) 
-   - GitHub: [github.com/quantrkucb/OrderBookVisualiser](https://github.com/quantrkucb/OrderBookVisualiser)
 
 6. **[Cal Hackathon] Robo-advisor for retail investors**
    - GitHub: [github.com/quantrkucb/CalHacksStockly](https://github.com/quantrkucb/CalHacksStockly/) 
    - Website: [devpost.com/software/stockly](https://devpost.com/software/stockly)
+
+7. 
+
+8. 
 """)
