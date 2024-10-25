d5920e42289683f820b2198a483fc467ae91eaf1
quantrkucb <119884363+quantrkucb@users.noreply.github.com>
2024-10-25 04:35:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ import streamlit as st
 st.header("Rishi Kumra: Recent Projects")
 
 st.write("""
-1. **Natural Language Processing on 10-K filings reports to forecast returns**
+1. **Natural Language Processing on 10-K Filings To Forecast Returns**
    - Streamlit: [nlp10kfilingsrk.streamlit.app](https://nlp10kfilingsrk.streamlit.app/)
    - GitHub: [github.com/quantrkucb/NLP_10K_Filings](https://github.com/quantrkucb/NLP_10K_Filings)
 
-2. **Multi-asset return prediction with feature importance**
-   - GitHub (Paper + Slides): https://github.com/quantrkucb/MultiAssetReturnPrediction 
+2.  **Order Book Visualizer**
+   - Streamlit: https://orderbookvisualiserrk.streamlit.app/ 
+   - GitHub: https://github.com/quantrkucb/OrderBookVisualiser
 
-3. **Tail-Risk hedging with financial derivatives (Bank of America)**
+3. **Tail-Risk hedging With Financial Derivatives (Bank of America)**
    - Streamlit: [nlp10kfilingsrk.streamlit.app](https://nlp10kfilingsrk.streamlit.app/)
    - GitHub: [github.com/quantrkucb/NLP_10K_Filings](https://github.com/quantrkucb/NLP_10K_Filings)
    - Paper: [Link to Paper]()
 
-4.  **Order Book Visualizer**
-   - Streamlit: https://orderbookvisualiserrk.streamlit.app/ 
-   - GitHub: https://github.com/quantrkucb/OrderBookVisualiser 
+4. **Multi-Asset Return Prediction With Feature Importance**
+   - GitHub (Paper + Slides): https://github.com/quantrkucb/MultiAssetReturnPrediction 
 
-5. **Portfolio Optimization: Analyzing and comparing models**
+5. **Portfolio Optimization: Modern Portfolio Theory Analysis**
    - Streamlit: [nlp10kfilingsrk.streamlit.app](https://nlp10kfilingsrk.streamlit.app/)
    - GitHub: [github.com/quantrkucb/NLP_10K_Filings](https://github.com/quantrkucb/NLP_10K_Filings)
 
