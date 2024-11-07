3fc0e91db5c00b0fc9d75436a349c8e358241dc1
quantrkucb <119884363+quantrkucb@users.noreply.github.com>
2024-11-07 13:52:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,31 @@ import streamlit as st
 st.header("Rishi Kumra: Recent Projects")
 
 st.write("""
+st.subheader("ML Applications")
 1. **Natural Language Processing on 10-K Filings To Forecast Returns**
    - Streamlit: [10K Filings Dashboard](https://nlp10kfilingsrk.streamlit.app/)
    - GitHub: [NLP_10K_GitHub](https://github.com/quantrkucb/NLP_10K_Filings)
 
-2. **Order Book Visualizer** 
-   - Streamlit: [Order Book Visualiser Dashboard](https://orderbookvisualiserrk.streamlit.app/)
-   - GitHub: [Order Book Visualiser GitHub](https://github.com/quantrkucb/OrderBookVisualiser)
+2. **Multi-Asset Return Prediction With Feature Importance**
+   - GitHub (Paper + Slides): [Multi-Asset Return Prediction: GitHub](https://github.com/quantrkucb/MultiAssetReturnPrediction)
 
-3. **Tail-Risk hedging With Financial Derivatives (Bank of America)**
+3. **[Cal Hackathon] Robo-advisor for retail investors**
+   - GitHub: [github.com/quantrkucb/CalHacksStockly](https://github.com/quantrkucb/CalHacksStockly/) 
+   - Website: [devpost.com/software/stockly](https://devpost.com/software/stockly)
+
+st.subheader("Portfolio Construction and Optimizations")
+4. **Tail-Risk hedging With Financial Derivatives (Bank of America)**
    - Streamlit: [Tail Risk Hedging Dashboard](https://tailriskhedging.streamlit.app/)
    - GitHub: [Tail Risk Hedging GitHub](https://github.com/quantrkucb/TailRiskHedgingWithDerivatives)
    - Paper: [Link to Paper](https://drive.google.com/file/d/1EbZj0dv3ZX-RDpnkuW1UwHaebh8Btdbl/view?usp=sharing)
-
-4. **Multi-Asset Return Prediction With Feature Importance**
-   - GitHub (Paper + Slides): [Multi-Asset Return Prediction: GitHub](https://github.com/quantrkucb/MultiAssetReturnPrediction)
 
 5. **Portfolio Optimization: Modern Portfolio Theory Applied**
    - Streamlit: [Markowitz/Black Litterman Dashboard](https://portfoliooptimizerrk.streamlit.app//)
    - GitHub: [github.com/quantrkucb/PortfolioOptimizer/](https://github.com/quantrkucb/PortfolioOptimizer/)
 
-6. **[Cal Hackathon] Robo-advisor for retail investors**
-   - GitHub: [github.com/quantrkucb/CalHacksStockly](https://github.com/quantrkucb/CalHacksStockly/) 
-   - Website: [devpost.com/software/stockly](https://devpost.com/software/stockly)
+st.subheader("Trading & Risk Management")
+6. **Order Book Visualizer** 
+   - Streamlit: [Order Book Visualiser Dashboard](https://orderbookvisualiserrk.streamlit.app/)
+   - GitHub: [Order Book Visualiser GitHub](https://github.com/quantrkucb/OrderBookVisualiser)
 
 """)
